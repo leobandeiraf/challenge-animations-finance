a17ce1919001e853af2ea472165b71137572fdb0
Leonardo Bandeira <leobandeiraf@gmail.com>
2022-11-27 22:33:37 -0300

    Closed intro rive animation feature

--- solutions/devsprint-leonardo-santos-5/leonardo-bandeira/AnimationsShowcase/Showcases/RiveSection/RiveIntroduction.swift
+++ solutions/devsprint-leonardo-santos-5/leonardo-bandeira/AnimationsShowcase/Showcases/RiveSection/RiveIntroduction.swift
@@ -3,12 +3,34 @@ import RiveRuntime
 import UIKit
 
 final class RiveIntroductionViewController: UIViewController {
+    // MARK: - Property(ies).
+    private let riveViewModel = RiveViewModel(fileName: "3321-6973-string-demo")
+    
+    // MARK: - Override(s).
+    override func loadView() {
+        super.loadView()
+        let riveView = riveViewModel.createRiveView()
+        view = riveView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setup()
+    }
+}
+
+// MARK: - ViewCodable.
+extension RiveIntroductionViewController: ViewCodable {
+    func setupSubviews() {}
+    
+    func setupConstraints() {}
+    
+    func setupExtraConfiguration() {
         view.backgroundColor = .systemBackground
     }
 }
 
+// MARK: - ShowcaseRow.
 struct RiveIntroductionShowcase: ShowcaseRow {
     var title: String {
         "Introdução ao Rive"
