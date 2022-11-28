5561c852ab9d0ac639ac61fe9d718730c91c6270
Leonardo Bandeira <leobandeiraf@gmail.com>
2022-11-27 23:00:09 -0300

    Added initial setup of challenge

--- solutions/devsprint-leonardo-santos-5/leonardo-bandeira/AnimationsShowcase/Showcases/RiveSection/RiveChallenge.swift
+++ solutions/devsprint-leonardo-santos-5/leonardo-bandeira/AnimationsShowcase/Showcases/RiveSection/RiveChallenge.swift
@@ -2,13 +2,58 @@ import Cartography
 import RiveRuntime
 import UIKit
 
+private extension RiveChallengeViewController.Size {
+    static let buttonSize: CGSize = .init(width: 236, height: 64)
+}
+
 final class RiveChallengeViewController: UIViewController {
+    // MARK: - Property(ies).
+    fileprivate enum Size {}
+    private let riveButton = RiveViewModel(fileName: "simple-button")
+    
+    // MARK: - Component(s).
+    private lazy var button: RiveView = {
+        let view = riveButton.createRiveView()
+        view.addSubview(label)
+        view.frame.size = Size.buttonSize
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    private lazy var label: UILabel = {
+        let label = UILabel()
+        label.text = "Devpass"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Override(s).
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        setup()
     }
 }
 
+// MARK: - ViewCodable.
+extension RiveChallengeViewController: ViewCodable {
+    func setupSubviews() {
+        view.addSubview(button)
+    }
+    
+    func setupConstraints() {
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        label.centerXAnchor.constraint(equalTo: button.centerXAnchor).isActive = true
+        label.centerYAnchor.constraint(equalTo: button.centerYAnchor).isActive = true
+    }
+    
+    func setupExtraConfiguration() {
+        view.backgroundColor = .white
+    }
+}
+
+// MARK: - ShowcaseRow.
 struct RiveChallengeShowcase: ShowcaseRow {
     var title: String {
         "Rive Challenge 🏆"
